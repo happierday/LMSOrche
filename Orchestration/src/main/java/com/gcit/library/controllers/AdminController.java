643125148f8fb4b27cd88fbdcd4010b76adb6435
Jian <jianchengw26@gmail.com>
2018-03-25 03:01:53 -0400

    change uri to path

--- Orchestration/src/main/java/com/gcit/library/controllers/AdminController.java
+++ Orchestration/src/main/java/com/gcit/library/controllers/AdminController.java
@@ -48,7 +48,7 @@ public class AdminController{
 	@RequestMapping(value="/books", method=RequestMethod.GET)
 	public ResponseEntity<Object> getBooks(@RequestHeader(value="Accept") String ct,@RequestParam(value="pageNo",required=false) Integer pageNo,
 			@RequestParam(value="search",required=false) String search){
-		UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(url+"/books")
+		UriComponentsBuilder builder = UriComponentsBuilder.fromPath(url+"/books")
 											.queryParam("pageNo", pageNo)
 											.queryParam("search", search);
 		HttpHeaders headers = new HttpHeaders();
